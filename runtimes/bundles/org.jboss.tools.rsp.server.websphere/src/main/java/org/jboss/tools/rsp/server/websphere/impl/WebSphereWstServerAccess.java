d58cccd8b0d62eb417039b9493eb216381395901
cabutchei <luropa_paz@hotmail.com>
2026-01-20 23:29:00 -0300

    no op debug target

---
+++ runtimes/bundles/org.jboss.tools.rsp.server.websphere/src/main/java/org/jboss/tools/rsp/server/websphere/impl/WebSphereWstServerAccess.java
@@ -0,0 +1,56 @@
+/*******************************************************************************
+ * Copyright (c) 2024 Red Hat, Inc. Distributed under license by Red Hat, Inc.
+ * All rights reserved. This program is made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is
+ * available at http://www.eclipse.org/legal/epl-v20.html
+ * 
+ * Contributors: Red Hat, Inc.
+ ******************************************************************************/
+package org.jboss.tools.rsp.server.websphere.impl;
+
+import org.jboss.tools.rsp.eclipse.core.runtime.CoreException;
+import org.jboss.tools.rsp.eclipse.core.runtime.IStatus;
+import org.jboss.tools.rsp.eclipse.core.runtime.Status;
+import org.jboss.tools.rsp.eclipse.wst.IWstIntegrationService;
+import org.jboss.tools.rsp.eclipse.wst.WSTServerContext;
+
+import com.ibm.ws.ast.st.v85.core.internal.WASServer;
+
+final class WebSphereWstServerAccess {
+	private WebSphereWstServerAccess() {
+		// utility class
+	}
+
+	static int getDebugPortNum(WSTServerContext context) throws CoreException {
+		WASServer server = getWASServer(context);
+		return server.getDebugPortNum();
+	}
+
+	static IWASConfigModelHelper createLocalWASConfigHelper(WSTServerContext context) {
+      return getWASServer(context).createLocalWASConfigHelper();
+	}
+
+	// static int getDebugPort(WSTServerContext context) {
+	// 	return WebSphereWstServerAccess.createLocalWASConfigHelper(context).
+	// }
+
+	static WASServer getWASServer(WSTServerContext context) throws CoreException {
+		if( context == null ) {
+			throw new CoreException(new Status(IStatus.ERROR, Activator.BUNDLE_ID, "Missing WST server context"));
+		}
+		IWstIntegrationService integration = Activator.getWstIntegrationService();
+		if( integration == null ) {
+			throw new CoreException(new Status(IStatus.ERROR, Activator.BUNDLE_ID, "WST integration service unavailable"));
+		}
+		String serverId = context.getServerHandle().getId();
+		org.eclipse.wst.server.core.IServer wstServer = integration.getFacade().getWstServer(serverId);
+		if( wstServer == null ) {
+			throw new CoreException(new Status(IStatus.ERROR, Activator.BUNDLE_ID, "WST server not found for id " + serverId));
+		}
+		WASServer wasServer = (WASServer) wstServer.loadAdapter(WASServer.class, null);
+		if( wasServer == null ) {
+			throw new CoreException(new Status(IStatus.ERROR, Activator.BUNDLE_ID, "Unable to load WASServer adapter"));
+		}
+		return wasServer;
+	}
+}
